2e308a7f8f7ac3b5d65ce8fe75528c621068f8b7
cjogles <jackson.ogles28@gmail.com>
2020-08-05 15:02:47 -0600

    finished lookup_table problem

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,6 @@
 # Your code here
-
+import math
+import random
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -9,18 +10,39 @@ def slowfun_too_slow(x, y):
 
     return v
 
-def slowfun(x, y):
+cache = {}
+inner_range = []
+
+def populate_cache():
+    list_of_coords = []
+    for i in range(2, 14):
+        for j in range(3, 6):
+            list_of_coords.append((i, j))
+    for i in (range(len(list_of_coords))):
+        cache[i] = slowfun_too_slow(list_of_coords[i][0], list_of_coords[i][1])
+
+def populate_inner_range():
+    for i in range(2, 14):
+        for j in range(3, 6):
+            inner_range.append((i, j))
+
+populate_cache()
+populate_inner_range()
+
+def slowfun(x, y, cache, inner_range):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    my_tuple = (x, y)
+    for i in range(len(inner_range)):
+        if inner_range[i] == my_tuple:
+            return cache[i]
 
-
-
+# **********************************************************8
 # Do not modify below this line!
 
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f'{i}: {x},{y}: {slowfun(x, y, cache, inner_range)}')
