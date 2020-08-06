337fa950a02dd81270767d33c7017f4894536dfd
cjogles <jackson.ogles28@gmail.com>
2020-08-06 14:10:20 -0600

    finished word_count problem

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,51 @@
+
+# " : ; , . - + = / \ | [ ] { } ( ) * ^ &
+
+import re
+
 def word_count(s):
     # Your code here
+    # first lowercase everything in string, and ignore characters I don't care about by deleting them
+    my_string = s.lower()
+    my_string = re.sub("\"", "", my_string)
+    my_string = re.sub("\[", "", my_string)
+    my_string = re.sub("\]", "", my_string)
+    my_string = re.sub("\{", "", my_string)
+    my_string = re.sub("\}", "", my_string)
+    my_string = re.sub("=", "", my_string)
+    my_string = re.sub("\.", "", my_string)
+    my_string = re.sub(":", "", my_string)
+    my_string = re.sub(";", "", my_string)
+    my_string = re.sub(",", "", my_string)
+    my_string = re.sub("-", "", my_string)
+    my_string = re.sub("\+", "", my_string)
+    my_string = re.sub("/", "", my_string)
+    my_string = re.sub("\\\\", "", my_string)
+    my_string = re.sub("\|", "", my_string)
+    my_string = re.sub("\(", "", my_string)
+    my_string = re.sub("\)", "", my_string)
+    my_string = re.sub("\*", "", my_string)
+    my_string = re.sub("\^", "", my_string)
+    my_string = re.sub("\&", "", my_string)
+    my_string = re.sub("  ", " ", my_string)
+    my_string = re.sub("  ", " ", my_string)
+    my_string = re.sub("  ", " ", my_string)
+    my_string = re.sub("  ", " ", my_string)
+    print(my_string)
+    # next, split the string into words and put them into a hash table with each key 
+    # equaling the given word, and its value initialized to zero
+    my_list = my_string.split()
+    my_dict = {}
+    for i in my_list:
+        if i not in my_dict:
+            my_dict[i] = 1
+        else:
+            my_dict[i] += 1
 
-
+    return my_dict
+    
+# my example...
+# word_count("here^& IS+ a a St-ri/ng ;T\\O *:Pr|ac,t(ICE) on. Why [ don't = \" you try{ } it out? ")
 
 if __name__ == "__main__":
     print(word_count(""))
